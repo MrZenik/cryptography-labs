a98761af63a62bcdd5cd1234044254a4168575df
Yevhen Berladyniuk <yevhen.berladyniuk@vitechteam.com>
2023-11-08 18:23:10 +0200

    Added original text print

--- AffineCipher/Program.cs
+++ AffineCipher/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string plaintext = "HELLO WORLD";
+Console.WriteLine("Огриніальний текст: " + plaintext);
+
 string ciphertext = AffineCipher.AffineCipher.EncryptMessage(plaintext.ToCharArray());
 Console.WriteLine("Зашифровано: " + ciphertext);
 
